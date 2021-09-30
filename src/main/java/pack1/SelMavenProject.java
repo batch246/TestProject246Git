cf66a47f68efd6ef3001f27c3fd1306622822084
getni <getni@DESKTOP-QTTOLM2.lan>
2021-09-29 23:01:33 -0400

    my commit

--- src/main/java/pack1/SelMavenProject.java
+++ src/main/java/pack1/SelMavenProject.java
@@ -4,7 +4,27 @@ public class SelMavenProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		{
+			
+			 int number=250;
+		        if(number > 0)
+		        {
+		            System.out.println(number+" is a positive number");
+		        }
+		        else if(number < 0)
+		        {
+		            System.out.println(number+" is a negative number");
+		        }
+		        else
+		        {
+		            System.out.println(number+" is neither positive nor negative");
+		        }
+		    }
+		}
 
+		
+		
+		
 	}
 
-}
+
